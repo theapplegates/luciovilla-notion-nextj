42883315e3c623b5dd418a5cedd6a88d4602a38b
Paul Applegate <paul@theapplegates.com>
2022-05-14 11:59:12 -0400

    first commit

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  i18n: {
+    locales: ['en-US'],
+    defaultLocale: 'en-US',
+  },
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    domains: ['pbs.twimg.com']
+  }
+}
