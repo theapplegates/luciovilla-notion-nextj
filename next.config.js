697e65057b86f8c703ec2f8c0c664311db1a4a9d
Paul Applegate <paul@theapplegates.com>
2022-05-14 16:30:21 +0000

    aaa

--- next.config.js
+++ next.config.js
@@ -4,7 +4,14 @@ module.exports = {
     defaultLocale: 'en-US',
   },
   images: {
+    domains: [
+      'www.notion.so',
+      'notion.so',
+      'images.unsplash.com',
+      'pbs.twimg.com',
+      's3.us-west-2.amazonaws.com',
+      'applegate-paul.mo.cloudinary.net'
+    ],
     formats: ['image/avif', 'image/webp'],
-    domains: ['pbs.twimg.com', 'applegate-paul.mo.cloudinary.net']
   }
 }
